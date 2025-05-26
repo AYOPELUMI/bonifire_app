d9d0541c23d2a8fec48d5b3f2875faff0de19ded
AYOPELUMI <ayopelumi2014@gmail.com>
2025-05-26 05:57:49 -0700

    added status bar transparency

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 import 'app/app_router.dart';
 import 'features/bonfire/provider/speech_provider.dart';
 import './res/theme.dart';
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize speech provider
